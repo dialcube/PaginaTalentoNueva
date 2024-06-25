8035b3e5117d81cb286652a4e2d31578e0e4af4b
whooker07 <100215241+whooker07@users.noreply.github.com>
2024-06-25 16:11:28 -0500

    cambios en usuario y creacion de sesiones

--- src/App.js
+++ src/App.js
@@ -2,17 +2,19 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import TalentoMejorada from "./Components/TalentoMejorada";
-
+import Sesiones from "./Components/Sesiones";
 import { BrowserRouter as Router,  Routes } from "react-router-dom";
 
 import './Components/TalentoMejorada.css';
+import CrearSesion from "./Components/CrearSesion";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <TalentoMejorada />
-        {/* <Inicio/> */}
+        {/* <Sesiones/>
+        <CrearSesion/> */}
         <Routes>
           
           {/* <Route path="/crearusuarios" element={<CrearUsuario />} /> */}
